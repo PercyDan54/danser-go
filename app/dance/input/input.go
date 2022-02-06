0a8c19a86f6aa0a87f345a2a7c064611a6434b52
PercyDan54 <50285552+PercyDan54@users.noreply.github.com>
2022-02-07 00:24:34 +0800

    Four key Auto

--- app/dance/input/input.go
+++ app/dance/input/input.go
@@ -4,6 +4,7 @@ import (
 	"github.com/wieku/danser-go/app/beatmap/objects"
 	"github.com/wieku/danser-go/app/graphics"
 	"github.com/wieku/danser-go/framework/math/mutils"
+	"math"
 )
 
 const singleTapThreshold = 140
@@ -15,7 +16,9 @@ type NaturalInputProcessor struct {
 	lastTime float64
 
 	wasLeftBefore  bool
+	index int
 	previousEnd    float64
+	releaseAt []float64
 	releaseLeftAt  float64
 	releaseRightAt float64
 }
@@ -24,8 +27,11 @@ func NewNaturalInputProcessor(objs []objects.IHitObject, cursor *graphics.Cursor
 	processor := new(NaturalInputProcessor)
 	processor.cursor = cursor
 	processor.queue = make([]objects.IHitObject, len(objs))
-	processor.releaseLeftAt = -10000000
-	processor.releaseRightAt = -10000000
+	processor.releaseAt = make([]float64, 4)
+	processor.releaseAt[0] = -10000000
+	processor.releaseAt[1] = -10000000
+	processor.releaseAt[2] = -10000000
+	processor.releaseAt[3] = -10000000
 
 	copy(processor.queue, objs)
 
@@ -41,7 +47,7 @@ func (processor *NaturalInputProcessor) Update(time float64) {
 			}
 
 			if processor.lastTime < g.GetStartTime() && time >= g.GetStartTime() {
-				startTime := g.GetStartTime()
+				//startTime := g.GetStartTime()
 				endTime := g.GetEndTime()
 
 				releaseAt := endTime + 50.0
@@ -52,27 +58,49 @@ func (processor *NaturalInputProcessor) Update(time float64) {
 					releaseAt = mutils.ClampF64(nTime-2, endTime+1, releaseAt)
 				}
 
-				shouldBeLeft := !processor.wasLeftBefore && startTime-processor.previousEnd < singleTapThreshold
-
-				if shouldBeLeft {
-					processor.releaseLeftAt = releaseAt
-				} else {
-					processor.releaseRightAt = releaseAt
-				}
-
-				processor.wasLeftBefore = shouldBeLeft
+				processor.releaseAt[int(math.Abs(float64(processor.index))) % 4] = releaseAt
 
 				processor.previousEnd = endTime
+				if(processor.index / 4 >= 1){
+				   processor.index = -2
+				}
 
 				processor.queue = append(processor.queue[:i], processor.queue[i+1:]...)
+				processor.index++
 
+				processor.lastTime = time
 				i--
 			}
 		}
 	}
 
-	processor.cursor.LeftKey = time < processor.releaseLeftAt
-	processor.cursor.RightKey = time < processor.releaseRightAt
+	switch (int(math.Abs(float64(processor.index - 1))) % 4){
+		case 0:
+			processor.cursor.LeftKey = true
+			processor.cursor.RightKey = false
+			processor.cursor.LeftMouse = time < processor.releaseAt[2]
+			processor.cursor.RightMouse = time < processor.releaseAt[3]
+		case 1:
+			processor.cursor.LeftKey = time < processor.releaseAt[0]
+			processor.cursor.RightKey = true
+			processor.cursor.LeftMouse = false
+			processor.cursor.RightMouse = false
+		case 2:
+			processor.cursor.LeftKey = false
+			processor.cursor.RightKey = time < processor.releaseAt[1]
+			processor.cursor.LeftMouse = true
+			processor.cursor.RightMouse = false
+		case 3:
+			processor.cursor.LeftKey = false
+			processor.cursor.RightKey = time < processor.releaseAt[1]
+			processor.cursor.LeftMouse = time < processor.releaseAt[2]
+			processor.cursor.RightMouse = true
+	}
 
-	processor.lastTime = time
+	if(time - processor.previousEnd > 100){
+		processor.cursor.LeftKey = false
+		processor.cursor.RightKey = false
+		processor.cursor.LeftMouse = false
+		processor.cursor.RightMouse = false
+	}
 }
